2f3b26292ded0f58099c8fa4985ecf0154cd0cab
Roben <robenthapaaa@gmail.com>
2025-12-04 03:14:11 -0800

    additionals

---
+++ Real_World_Project_Structure/src/utils/exception.py
@@ -0,0 +1,37 @@
+import sys
+from utils.logger import logging
+
+def error_message_detail(error, error_detail: sys):
+    """
+    Extracts detailed error information:
+    - File name where the error occured
+    - Line number
+    - Original error message
+    """
+
+    exc_type, exc_obj, exc_tb = error_detail.exc_info()
+
+    # Safety check in case exc_info() returns None (eg. manually raised errors)
+    if exc_tb is None:
+        return f"Error: {str(error)}"
+    
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+
+    return f"Error occured in python script [{file_name}] at line [{line_number}] : {str(error)}"
+
+
+class CustomException(Exception):
+    """
+    Custom Exception class for better error logging.
+    """
+
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+        # Log actomatically when execption is created
+        logging.error(self.error_message)
+
+    def __str__(self):
+        return self.error_message
