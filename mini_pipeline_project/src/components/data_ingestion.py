5939759f943e6451f8db248c928324fa91c99b43
Roben <robenthapaaa@gmail.com>
2025-12-04 03:09:35 -0800

    adding project

---
+++ mini_pipeline_project/src/components/data_ingestion.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_openml
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+
+    def initiate_data_ingestion(self):
+        logging
