d4e2599d46464659dcf267fec7972dbb9a4638e5
Roben <robenthapaaa@gmail.com>
2025-12-05 02:21:57 -0800

    bug fixing

--- Real_World_Project_Structure/src/pipeline/train_pipeline.py
+++ Real_World_Project_Structure/src/pipeline/train_pipeline.py
@@ -3,9 +3,9 @@ import os
 from src.utils.exception import CustomException
 from src.utils.logger import get_logger
 
-from components.data_ingestion import DataIngestion
-from components.data_transformation import DataTransformation
-from components.model_trainer import ModelTrainer
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 logger = get_logger(__name__)
 
