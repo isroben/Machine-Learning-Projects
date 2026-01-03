742f37ed2e901f6414565b6ed1045e543f3a9f76
Roben <robenthapaaa@gmail.com>
2026-01-03 15:24:23 +0545

    model trainer

--- HandWritten_Digits_Classification_Project/src/components/model_trainer.py
+++ HandWritten_Digits_Classification_Project/src/components/model_trainer.py
@@ -1,2 +1,34 @@
 import sys
-import pandas as pd
+import os
+from dataclasses import dataclass
+
+from src.utils.exception import CustomException
+from src.utils.logger import get_logger
+from src.utils.utility import save_object
+from src.components.model_evaluation import evaluate_models
+
+from sklearn.ensemble import (AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier)
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+
+logger = get_logger(__name__)
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_arr, test_arr):
+        try:
+            logger.info("Splitting training and testing input data.")
+
+            X_train, y_train, X_test, y_test = ()
+        except:
+            pass
