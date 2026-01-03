c25330fe0d9962c13a3db6b29deabbd5ffa19876
Roben <robenthapaaa@gmail.com>
2026-01-03 17:02:49 +0545

    training pipeline completed

--- HandWritten_Digits_Classification_Project/src/pipeline/train_pipeline.py
+++ HandWritten_Digits_Classification_Project/src/pipeline/train_pipeline.py
@@ -20,14 +20,26 @@ def trainingPipeline():
         logger.info("Data ingestion completed.")
 
         # Data transformation
+        logger.info("Data transformation process initialized.")
         transformation = DataTransformation()
-        train_arr, test_arr, _ = transformation.initiate_data_transformation(train_data_path, test_data_path)
-
-        # Model Training
-        trainer = ModelTrainer()
-        best_model, best_model_name, model_report = trainer.initiate_model_trainer(train_arr, test_arr)
-
+        X_train, y_train, X_test, y_test = transformation.initiate_data_transformation(train_set, test_set)
         logger.info("Data transformation completed.")
 
+        # Model Training
+        logger.info("Model trainer initialized")
+        trainer = ModelTrainer()
+        best_model, best_model_name, model_report = trainer.initiate_model_trainer(X_train, y_train, X_test, y_test)
+
+        logger.info("Model training comopleted.")
+        logger.info(f"Best model selected: {best_model_name}")
+        logger.info(f"Performance Report: {model_report[best_model_name]}")
+
+        print("\n================ TRAINING COMPLETED =================")
+        print(f"Best Model\t\t: {best_model_name}")
+
+        return best_model, best_model_name, model_report
+    
     except Exception as e:
         raise CustomException(e, sys)
+    
+trainingPipeline()
